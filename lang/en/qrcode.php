f388e13fc7c7d6313962b65bcfbc447796cf514a
Michael Backus <mbbackus@gmail.com>
2018-09-18 06:06:18 +0200

    This version of qrcode is minimal but functional. Create an assignment, and you can upload a grade using the app provided you have setup the web service.

---
+++ lang/en/qrcode.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for qrcode
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_qrcode
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'qrcode';
+$string['modulenameplural'] = 'qrcodes';
+$string['modulename_help'] = 'Use the qrcode module for... | The qrcode module allows...';
+$string['qrcode:addinstance'] = 'Add a new qrcode';
+$string['qrcode:submit'] = 'Submit qrcode';
+$string['qrcode:view'] = 'View qrcode';
+$string['qrcodefieldset'] = 'Custom example fieldset';
+$string['qrcodename'] = 'qrcode name';
+$string['qrcodename_help'] = 'This is the content of the help tooltip associated with the qrcodename field. Markdown syntax is supported.';
+$string['qrcode'] = 'qrcode';
+$string['pluginadministration'] = 'qrcode administration';
+$string['pluginname'] = 'qrcode';
